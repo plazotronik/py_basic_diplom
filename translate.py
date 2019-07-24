d0ff9f74d460e2c0fe40fe23b2241820de45f982
Vladimir Eroshenko <admin@it-vi.ru>
2019-07-24 15:08:34 +0300

    Add: translation of error messages

--- translate.py
+++ translate.py
@@ -5,7 +5,6 @@ import requests
 
 URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 KEY_API = 'trnsl.1.1.20190704T182934Z.17f33d8db55385c6.e6d41260c9ccabfba9197455fd6d6679fec9bf38'
-LEGAL = f'\n{"=" * 67}\nПереведено сервисом «Яндекс.Переводчик» http://translate.yandex.ru/\n'
 
 
 def translate(text):
