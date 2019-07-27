c3ed65501917377882becdaa95cf355f9b3f852e
Vladimir Eroshenko <admin@it-vi.ru>
2019-07-27 15:44:27 +0300

    release 2.0 (New: meet minimum requirements)

--- spy_games.py
+++ spy_games.py
@@ -11,7 +11,7 @@ import os
 
 URL = 'https://api.vk.com/method/'
 VER = '5.101'
-TOKEN = 'be427a11b7c20f57643d8c52d87e5f357ad196f90e59d5752449fa46f015104781de59787f9706b18aac2'
+TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 
 
 def make_output_dir():
@@ -42,45 +42,6 @@ def write_json(text, id, members=0, mode='wt'):
     txt = json.dumps(text, sort_keys=True, indent=4, ensure_ascii=False)
     with open(out_file, mode=mode, encoding='utf-8-sig') as file:
         file.write(txt)
-
-
-def exec_spy_groups(groups, members=0):
-    '''
-    get spy groups with execute
-
-    :param groups:
-    list ids all groups
-    :param members:
-    count member friends of group
-    :return:
-    list ids spy groups
-    '''
-    method = 'execute'
-    groups_str = ",".join(str(i) for i in groups)
-    code = '''
-    var i = 0;
-    var members = ''' + str(members) + ''';
-    var gidsbefore = [''' + groups_str + '''];
-    var gidsafter = [];
-    while (i < gidsbefore.length) {
-    var ids = gidsbefore.pop();
-    var resp = API.groups.getMembers({"group_id": ids , "filter": "friends"});
-    if ((resp.count == 0) || (resp.count < members)) {
-    gidsafter.push(ids);
-    }
-    i = i + 1;
-    }
-    return gidsafter;
-    '''
-    parametrs = {
-        'code': code.strip(),
-        'v': VER,
-        'access_token': TOKEN,
-    }
-    sleep(0.2)
-    response = requests.get(url=f'{URL}{method}', params=parametrs)
-    resp = response.json()['response']
-    return resp
 
 
 class UserVK:
@@ -136,47 +97,6 @@ class UserVK:
             self.fio = self.family + ' ' + self.name
             self.url = f'https://vk.com/{self.domain}'
 
-    def getfriends(self):
-        """
-        Вывод всех друзей пользователя без создания из них экземпляров класса (ибо время выполнения кода - дорого).
-        Возвращает список словарей.
-        Таймаут на выполнение запроса 0.2 секунды достаточно для вхождения в ограничение 3 запроса в секунду,
-        т.к. дополнительно учитывается время выполнения кода и самого запроса...
-
-        :return
-        list ids of all friends
-        """
-        if self.delete:
-            return print(f'{" " * 3}Невозможно найти друзей - пользователь c id{self.user_id} '
-                         f'удален или заблокирован.')
-        elif self.close & (not self.can_access_closed):
-            return print(f'{" " * 3}Невозможно найти друзей - приватный профиль пользователя.')
-        else:
-            method = 'friends.get'
-            parametrs = {
-                'user_id': self.user_id,
-                'fields': 'nickname',
-                'v': VER,
-                'access_token': TOKEN,
-            }
-            try:
-                sleep(0.2)
-                t = tqdm.tqdm(iterable=None, desc='Progress', total=1, unit=' parrots', leave=False)
-                response = requests.get(url=f'{URL}{method}', params=parametrs)
-                resp = response.json()['response']['items']
-            except KeyError:
-                resp = response.json()["error"]
-                KE = f'\nПроизошла ошибка обращения к ключу. Возможно сервер вернул не то, что ожидалось.' \
-                    f'\n{" "*3}код_ошибки: {resp["error_code"]}\n{" "*3}описание: {translate(resp["error_msg"])}'
-                print(KE)
-                return KE
-            else:
-                print(f'\nДрузья пользователя {self.fio} ({self.url}):')
-                for i, rsp in enumerate(resp):
-                    t.update()
-                    print(f'{" " * 3}{i+1}) {rsp["last_name"]} {rsp["first_name"]} - https://vk.com/id{rsp["id"]}')
-                return resp
-
     def getgroups(self):
         '''
         get list groups for self
@@ -209,7 +129,7 @@ class UserVK:
             else:
                 return resp
 
-    def getspygroups(self, members=0):
+    def getspygroups(self, members=1):
         '''
         method for get spy groups
 
@@ -229,14 +149,19 @@ class UserVK:
             t.update(13)
             lst_spy_gid = []
             lst_tmp = self.getgroups()
-            num1 = len(lst_tmp) / 25
-            if len(lst_tmp) <= 25:
-                lst_spy_gid = exec_spy_groups(lst_tmp, members)
-                t.update(1)
-            else:
-                for i in range(num1.__round__() + 1):
-                    lst_spy_gid.extend(exec_spy_groups(lst_tmp[:25], members))
-                    del lst_tmp[:25]
+            for group in lst_tmp:
+                method = 'groups.getMembers'
+                parametrs = {
+                    'group_id': group,
+                    'filter': 'friends',
+                    'v': VER,
+                    'access_token': TOKEN,
+                }
+                sleep(0.3)
+                t.update(2)
+                response = requests.get(url=f'{URL}{method}', params=parametrs)
+                if response.json()['response']['count'] < members:
+                    lst_spy_gid.append(group)
                     t.update(2)
             lst_spy = []
             if len(lst_spy_gid) == 0:
@@ -251,63 +176,6 @@ class UserVK:
                 print('Колличество сообществ: ', len(lst_spy), 'шт.')
                 pprint(lst_spy)
                 return lst_spy
-
-    def __and__(self, other):
-        '''
-        Вывод общих друзей двух пользователей и создание из них экземпляров класса.
-        Возвращает словарь.
-        Таймаут на выполнение запроса 0.2 секунды достаточно для вхождения в ограничение 3 запроса в секунду,
-        т.к. дополнительно учитывается время выполнения кода и самого запроса...
-
-        :param other:
-        иной экземпляр класса UserVK
-        :return:
-        словарь общих друзей
-        '''
-        try:
-            if self.delete:
-                return print(f'\nНевозможно найти общих друзей. Пользователь c id{self.user_id} удален или заблокирован.')
-            elif other.delete:
-                return print(f'\nНевозможно найти общих друзей. Пользователь c id{other.user_id} удален или заблокирован.')
-            else:
-                method = 'friends.getMutual'
-                parametrs = {
-                    'source_uid': self.user_id,
-                    'target_uid': other.user_id,
-                    'v': VER,
-                    'access_token': TOKEN,
-                }
-                if self == other:
-                    print('\nСравнивать себя со своим псевдонимом не очень то логично...)) '
-                          '\nДа и займет уйму времени на создание экземпляров класса. '
-                          'Лучше воспользоваться методом .getfriends()\n'
-                          'Например, iam.getfriends() или usr_0000000.getfriends()')
-                else:
-                    try:
-                        sleep(0.2)
-                        response = requests.get(url=f'{URL}{method}', params=parametrs)
-                        resp = response.json()['response']
-                    except KeyError:
-                        resp = response.json()["error"]
-                        KE = f'\nПроизошла ошибка обращения к ключу. Возможно сервер вернул не то, что ожидалось.' \
-                            f'\n{" "*3}код_ошибки: {resp["error_code"]}\n{" "*3}описание: {translate(resp["error_msg"])}'
-                        print(KE)
-                        return KE
-                    else:
-                        # lst_friends = []
-                        dict_friends = {}
-                        print(f'\nОбщие друзья у пользователей {self.fio} и {other.fio}:')
-                        for ind, rsp in enumerate(resp):
-                            globals()[f'usr_{rsp}'] = UserVK(rsp)
-                            dict_friends.update({f'usr_{rsp}': globals()[f'usr_{rsp}'].__str__()})
-                            # lst_friends.append(globals()[f'usr_{rsp}'].__str__())
-                            print(f'{" " * 3}{ind+1}) usr_{rsp}: {globals()[f"usr_{rsp}"]}')
-                        return dict_friends
-                        # return lst_friends
-        except AttributeError:
-            AtbE = f'\nВы ошиблись в написании одного из пользователей. Проверьте ввод и повторите позднее.'
-            print(AtbE)
-            return AtbE
 
     def __str__(self):
         return f'{self.fio} - {self.url}'
@@ -352,7 +220,7 @@ class GroupVK:
         return f'"name": {self.name}, "gid": {self.gid}, "members_count": {self.membs}, "url": {self.url}'
 
 
-def input_id_user(members=0):
+def input_id_user(members=1):
     '''
     input id(s) user(s) (+ members) -> print spy groups & create file with spy groups
 
@@ -361,34 +229,13 @@ def input_id_user(members=0):
     :return:
     print spy groups & create file with spy groups
     '''
-    user_id = str(input('Введите id пользовател(я/ей): ')).lower()
-    if ',' in user_id:
-        print('\nНаходим сообщества для каждого пользователя.')
-        for user in list(user_id.split(',')):
-            user = str(user.strip())
-            try:
-                tmp = UserVK(user)
-                print(f"\nСообщества пользователя {tmp.fio} с id {tmp.user_id}:")
-                write_json(tmp.getspygroups(members), user, members)
-            except Exception:
-                pass
-    elif ' ' in user_id:
-        print('\nНаходим сообщества для каждого пользователя.')
-        for user in list(user_id.split(' ')):
-            user = user.strip()
-            try:
-                tmp = UserVK(user)
-                print(f"\nСообщества пользователя {tmp.fio} с id {tmp.user_id}:")
-                write_json(tmp.getspygroups(members), user, members)
-            except Exception:
-                pass
-    else:
-        try:
-            tmp = UserVK(user_id)
-            print(f"\nСообщества пользователя {tmp.fio} с id {tmp.user_id}:")
-            write_json(tmp.getspygroups(members), user_id, members)
-        except Exception:
-            pass
+    user_id = str(input('Введите id пользователя: ')).lower()
+    try:
+        tmp = UserVK(user_id)
+        print(f"\nСообщества пользователя {tmp.fio} с id {tmp.user_id}:")
+        write_json(tmp.getspygroups(members), user_id, members)
+    except Exception:
+        pass
 
 
 def very_main():
